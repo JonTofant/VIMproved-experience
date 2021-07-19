f2e1f8954b3a03ea159f9130a1031ae42d0b8fa5
Jon Tofant <75683804+JonTofant@users.noreply.github.com>
2021-07-19 17:40:22 +0200

    Added keymaps for git

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -34,3 +34,7 @@ vim.api.nvim_set_keymap('n', '<C-t>', ':ToggleTerm<CR>',{noremap = true, silent 
 vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<CR>',{noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<leader>fg', ':Telescope git_status<CR>',{noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<leader>fw', ':Telescope current_buffer_fuzzy_find<CR>',{noremap = true, silent = true})
+
+--Git
+vim.api.nvim_set_keymap('n', '<leader>gs', ':vsp<CR> :Gina status<CR>',{noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>gc', ':vsp<CR> :Gina commit<CR>',{noremap = true, silent = true})
